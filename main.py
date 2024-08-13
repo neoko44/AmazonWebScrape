f1882ada37e61d5d778cd8fb900bb52bfeaed66c
neoko44 <alperen12010@windowslive.com>
2024-08-14 02:48:55 +0300

    Base Created

--- main.py
+++ main.py
@@ -1,58 +1,115 @@
+import json
 import re
-import csv_opt
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from entities import Product, Fields
+from entities import Product, DataResult
 
-binance_SITE = 'https://www.binance.com/tr/markets/overview'
-amazon_SITE = 'https://www.amazon.com/Ice-Roller-Face-Massager-Puffiness/dp/B088WBLF5Z/ref=sr_1_1_sspa?dib=eyJ2IjoiMSJ9.tc2iVqhd9TxbTatkh28QSXkNiBl11hwlKrb09QXHK3fyCOGSbyoen5GaKsqXsQd7vrtl83ofENgDiyG3h7oaJThAYUsPIhFcvLunm_B9rI6cOCpThbTwjN2gzpTPKnKzXJhThdOYAzk8MbuP1OGIGY5SiFMKTWh37_m4ZeRAOSh2uugL6LAnjGpSJwDfU-RVV8b1UQBF4Biei5AMk8VhPRV6omhGWTL3qCev58w2oO9F_R8Rca2sfDq1CLeqETzMhtd3SonIrXx6Rfk8gDpGH4f4E_qOxcvJ2534GbmP1MY.75RxQzXtKH8v80QZCoDwEV90rZTgp-GPOIn25ClU4Nk&dib_tag=se&keywords=products&qid=1722820201&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1'
+# Amazon product url to scrape
+amazon_product_URL = 'https://www.amazon.com/Anker-5-Pack-Charger-MacBook-Samsung/dp/B0CFZDHRPP?ref=dlx_deals_dg_dcl_B0CFZDHRPP_dt_sl14_d5'
+
+# region driver options
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0",
+    "User-Agent": "ADD YOUR USER AGENT HERE",
     "Accept-Encoding": "gzip, deflate",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "DNT": "1",
     "Connection": "close",
     "Upgrade-Insecure-Requests": "1"}
 
-prod_list = []
+options = webdriver.EdgeOptions()
 
-def create_table(file_name):
-    fields = Fields().as_list()
-    is_table = csv_opt.create_csv(file_name, fields)
+for h in headers:
+    options.add_argument(h)
+options.add_argument('--headless')
 
-    return is_table
+driver = webdriver.Edge(options=options)
 
-def amazon_get_product(link):
-    options = webdriver.EdgeOptions()
-    options.add_argument('--headless')
 
-    driver = webdriver.Edge(options=options)
-    driver.get(link)
+# endregion
 
+# region Method for getting amazon product by URL
+def amazon_get_product(url):
+    # Trying to load webpage by URL.
+    # If throws an exception,
+    # returns false in DataResult
+    try:
+        driver.get(url)
+    except Exception as e:
+        return DataResult(status=False,
+                          message=e,
+                          data=None)
+
+    # soup1 trying to get page source by parsing currently working driver
     soup1 = BeautifulSoup(driver.page_source, 'html.parser')
+
+    # soup2 prettifying the page source we just get
     soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
 
-    title = soup2.find(id='productTitle').get_text().strip()
-    category = soup2.find(class_='a-unordered-list a-horizontal a-size-small')
-    category = re.sub(r"\s+ ", "", category.get_text(), flags=re.UNICODE).replace('›', ' -- ').strip()
+    # Even URL is not correct, BeautifulSoup gets a page source.
+    # We must search for something exist on our Amazon product page,
+    # so we are looking for [id = 'productTitle']
+    # if not exist returns false in DataResult
+    title = soup2.find(id='productTitle')
 
+    if title is None:
+        return DataResult(status=False,
+                          message="Please enter valid URL",
+                          data=None)
+
+    # If productTitle exists:
+    # get text of element,
+    # remove spaces at the beginning and at the end
+    title = title.get_text().strip()
+
+    # Get category node
+    category = soup2.find(class_="a-unordered-list a-horizontal a-size-small")
+
+    # Some amazon products do not have category node.
+    # If there is no category node we will return ""
+    if category:
+        category = re.sub(r"\s+ ", "", category.get_text(), flags=re.UNICODE).replace('›', ' -- ').strip()
+
+    # Get Price element
     get_price = soup2.find('span', class_="a-price aok-align-center reinventPricePriceToPayMargin priceToPay")
 
+    # Price example in get_price : 1.015,64 TL
+
+    # Get whole price : 1015
     whole_price = get_price.find('span', class_="a-price-whole").get_text().replace('.', '').replace(',', '').strip()
+
+    # Get fraction price : 64
     fraction_price = get_price.find('span', class_="a-price-fraction").get_text().strip()
+
+    # Get currency : TL
     currency = get_price.find(class_="a-price-symbol").get_text().strip()
 
+    # Total price : 1015.64
     total_price = whole_price + '.' + fraction_price
 
-    return Product(title=title, price=total_price, currency=currency, category=category)
+    # Create Product
+    data = Product(title=title,
+                   price=total_price,
+                   currency=currency,
+                   category=category or "").to_dict()
+
+    # Return Product and return in DataResult
+    return DataResult(data=data,
+                      message="success",
+                      status=True)
+
+
+# endregion
 
 
 if __name__ == '__main__':
-    table = create_table('Test')
+    # returns DataResult object
+    prod = amazon_get_product(amazon_product_URL)
 
-    data = amazon_get_product(amazon_SITE)
+    # Convert DataResult object to JSON
+    as_json = json.dumps(prod.to_dict(),
+                         default=str,
+                         ensure_ascii=False,
+                         indent=4)
 
-    obj = Product(title=data.title, price=data.price, currency=data.currency, category=data.category)
-    prod_list.append(obj.as_data())
-
-    csv_opt.append_to_csv('Test', prod_list)
+    print(as_json)
